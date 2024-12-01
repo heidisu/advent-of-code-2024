c40dee2ea85fabed5e991da813aa20da10c3d6f4
Heidi Mork <heidimork@yahoo.no>
2024-12-01 06:26:02 +0100

    day1

---
+++ day1/day1.fsx
@@ -0,0 +1,28 @@
+
+open System.IO
+
+let readFile () = 
+    let lines = 
+        File.ReadLines "input.txt"
+        |> Seq.toList
+        |> List.map (fun l -> 
+                        let parts = l.Split(" ")
+                        (int parts[0], int parts[Array.length parts - 1])
+                        )
+    (lines |> List.map fst, lines |> List.map snd)               
+
+let (l1, l2) = readFile ()
+let task1 =
+    let l1s = List.sort l1
+    let l2s = List.sort l2
+    List.zip l1s l2s
+    |> List.map (fun (a, b) -> abs (a - b))
+    |> List.sum
+let task2 =
+    List.fold (fun acc v -> 
+            let res = l2 |> List.filter (fun i -> i = v) |> List.length
+            acc + (res * v)    
+            ) 0 l1
+
+printfn $"Task 1: {task1}" // 1530215
+printfn $"Task 2: {task2}" // 26800609
